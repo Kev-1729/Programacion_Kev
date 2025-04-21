787ec2102fd7cbf1fd9c54f3efa6c326f821469a
Kevin Tupac Agüero <96688861+Kev-1729@users.noreply.github.com>
2025-04-21 01:07:05 -0500

    Update punteros.cpp

--- Punteros/punteros.cpp
+++ Punteros/punteros.cpp
@@ -13,6 +13,6 @@ int main(){
 	
 	cout<<p; // Muestra la direccion de memoria: 0x6ffe14
   
-  cout<<*p; // Muesta el valor de la variable porque se le agrego *, en este caso: 2
+  	cout<<*p; // Muesta el valor de la variable porque se le agrego *, en este caso: 2
 	return 0;
 }
